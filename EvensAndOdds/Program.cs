d7ab336cfe28fe3fa90abee2c4f9911aaa9a17f5
Greg Smith <gregsmith446@gmail.com>
2019-01-17 11:29:16 -0500

    completed challenge using if else statements

--- EvensAndOdds/Program.cs
+++ EvensAndOdds/Program.cs
@@ -10,6 +10,23 @@ namespace EvensAndOdds
     {
         static void Main(string[] args)
         {
+                Console.WriteLine("give int between 1 and 100");
+                int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 != 0)
+            {
+                Console.WriteLine("{0} is Weird", N);
+            }
+            else if (N % 2 == 0 && N <= 5 && N >= 2)
+            {
+                Console.WriteLine("{0} is Not Weird", N);
+            } else if (N % 2 == 0 && N <= 20 && N >= 6)
+            {
+                Console.WriteLine("{0} is Weird", N);
+            } else if (N % 2 == 0 && N > 20)
+            {
+                Console.WriteLine("{0} is Not Weird", N);
+            }
         }
     }
 }
